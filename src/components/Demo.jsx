7ad661d40412cf9977699281ed9e331cb2a2404a
khuephamy <khuephumy19@gmail.com>
2023-05-07 11:58:12 -0500

    feature hadleSubmit function

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,14 +2,26 @@ import { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from "../assets";
 
+import { useLazyGetSummaryQuery } from "../services/article";
+
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
 
-  const handleSubmit = async () => {
-    alert("Submited");
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      setArticle(newArticle);
+      console.log(newArticle)
+    }
   };
   const handleOnChange = (e) => {
     setArticle({ ...article, url: e.target.value });

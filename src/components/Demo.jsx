2cf3dc0126e57a5f17d0a2ccc5589d5105d67583
khuephamy <khuephumy19@gmail.com>
2023-05-07 11:22:58 -0500

    feature handleOnChange function

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,20 +3,33 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async () => {
+    alert("Submited");
+  };
+  const handleOnChange = (e) => {
+    setArticle({ ...article, url: e.target.value });
+  };
   return (
     <section className="mt-16 w-full max-2-xl">
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center" onSubmit={() => {}}>
+        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
           <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
           <input
             type="url"
             placeholder="Enter an URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={handleOnChange}
             required
             className="url_input peer"
           />
-          <button type="submit" className="submit_btn">🚀</button>
+          <button type="submit" className="submit_btn">
+            🚀
+          </button>
         </form>
       </div>
     </section>
